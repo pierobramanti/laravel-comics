76acf65541b80a8c1f7ca3a119700345fcacce2d
pierobramanti <piero.bramanti94@gmail.com>
2024-09-24 21:18:09 +0200

    [UPD] finished working on header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Comics</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel DC comics</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
-    
+    @include('layouts.partials.header')
+    <main>
+        @yield("content")
+    </main>
+    @include('layouts.partials.footer')
 </body>
 </html>
